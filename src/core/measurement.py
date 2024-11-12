cc449ce79788a1df2b5643b29e89c4f481edefaf
Natsoulas <nnatsoulas@gmail.com>
2024-11-12 10:48:14 -0700

    added copyright

--- src/core/measurement.py
+++ src/core/measurement.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2024 Niko Natsoulas
+# 
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 import numpy as np
 
 def measurement_model(state: np.ndarray, noise: np.ndarray) -> np.ndarray:
--- src/utils/plotting.py
+++ src/utils/plotting.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2024 Niko Natsoulas
+# 
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 import numpy as np
 import matplotlib.pyplot as plt
 from typing import Tuple
